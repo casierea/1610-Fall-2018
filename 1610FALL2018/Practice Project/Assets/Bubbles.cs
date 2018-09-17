7417f9329c7d01377eb833802049a518186ccbf4
Casandra Roell <dancingmongoose@gmail.com>
2018-09-16 20:00:48 -0600

    Scripting level 1
    Practiced creating scripts, dog, animal, bubble, bird, duck

---
+++ 1610FALL2018/Practice Project/Assets/Bubbles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubbles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		print("I am bubbles of happiness");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1610FALL2018/Practice Project/Assets/Duck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : Bird {
+
+	// Use this for initialization
+	void Start () {
+		print("Cluck Cluck you are a duck.Quack Quack Quack, you get a smack!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
