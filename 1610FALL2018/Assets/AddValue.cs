dc96141adaddc041f6b5ddee2e5e4eacaf1633d8
Unknown <dancingmongoose@gmail.com>
2018-09-19 15:45:22 -0600

    vlass

---
+++ 1610FALL2018/Assets/AddValue.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class AddValue : ScriptableObject
+{
+
+
+	public FloatData ValueObj;
+	public FloatData MaxValue;
+	public FlaotData MinValue;
+	
+	public UnityEvent Event;
+	public UnityEvent EventMax;
+	
+	public void AddValueToObj(FloatData data)
+	{
+		ValueObj.Value += data.Value;
+		if (ValueObj.Value <= MaxValue.Value)
+		{
+			EventMax.invoke();
+		}
+	}
+}
---
+++ 1610FALL2018/Assets/MouseDownBehaviour.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseDownBehaviour : MonoBehaviour
+
+{
+
+	public UnityEvent Event;
+
+	private void OnMouseDown()
+	{
+		Event.Invoke();
+	}
+
+}
