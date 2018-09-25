e5b6a5ee7e660d1436d87872d9d14254fa811be1
Casandra Roell <dancingmongoose@gmail.com>
2018-09-24 21:05:26 -0600

    New Unity Project for assignment 01
    cats, ships, kittens, transform object, inheritence

---
+++ 1610FALL2018/Assets/Scenes/New Unity Project/Assets/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		print("I am a cat. Meow.");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1610FALL2018/Assets/Scenes/New Unity Project/Assets/Kitten.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitten : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		print("I am a kitten. I also meow.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
